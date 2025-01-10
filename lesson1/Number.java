0c88fbf8a8b58b313666dfebc4c804c147947bf7
mizuamedesu <97249122+mizuamedesu@users.noreply.github.com>
2025-01-10 18:58:05 +0900

    update

--- lesson1/Number.java
+++ lesson1/Number.java
@@ -1,19 +1,18 @@
 public class Number {
     public static void main(String[] args) {
-        // UTID-13が自分は0012024121173
         int d1 = 0,
             d2 = 0,
-            d3 = 1,
-            d4 = 2,
+            d3 = 0,
+            d4 = 0,
             d5 = 0,
-            d6 = 2,
-            d7 = 4,
-            d8 = 1,
-            d9 = 2,
-            da = 1,
-            db = 1,
-            dc = 7,
-            dd = 3;
+            d6 = 0,
+            d7 = 0,
+            d8 = 0,
+            d9 = 0,
+            da = 0,
+            db = 0,
+            dc = 0,
+            dd = 0;
         int v1 = d1 + d3 + d5 + d7 + d9 + db,
             v3 = d2 + d4 + d6 + d8 + da + dc,
             vv = ( v3 * 3 + v1 + dd ) % 10;
