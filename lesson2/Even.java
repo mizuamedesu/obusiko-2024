049b2bd110313e84c77d8769bcc5e7575a3bd4a9
mizuamedesu <97249122+mizuamedesu@users.noreply.github.com>
2025-01-14 21:33:49 +0900

    第二回の宿題やったよ

---
+++ lesson2/Even.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class Even {
+    public static void main(String[] args) {
+        int i = 0 ;
+        while (i < 10) {
+            System.out.println("i="+(i * 2));
+            i++;
+        }
+    }
+}
+
---
+++ lesson2/Square.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+public class Square {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 10) {
+            System.out.println(i * i);
+            i++;
+        }
+    }
+}
