743b122f7859e3d5d23902d68d04a547b45c57e6
mizuamedesu <97249122+mizuamedesu@users.noreply.github.com>
2025-01-21 15:02:14 +0900

    feat: 第3回課題やった

---
+++ lesson3/Point.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+public class Point {
+    int x, y;
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    void printPoint() {
+        System.out.println("座標(" + x + ", " + y + ")");
+    }
+}
